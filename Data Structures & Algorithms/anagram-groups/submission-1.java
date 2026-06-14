08aad13a16f016902a82be08b35ca558d1621d32
Abeer Sharif <abeersharif05@gmail.com>
2026-06-14 21:38:24 +0530

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+
+            int[] freq = new int[26];
+
+            // Count characters
+            for (char c : word.toCharArray()) {
+                freq[c - 'a']++;
+            }
+
+            // Convert frequency to string key
+            String key = Arrays.toString(freq);
+
+            // Add to map
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
